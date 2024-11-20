63eadd68eed6bc70d300e56d099a9bd40e26ad9a
Debora Alejandra Garcia Cruz <deboraalejandragarciacruz@gmail.com>
2024-11-20 09:01:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ print("hola Mundo")
 
 print('Este es un mensaje con comillas simples.')
 print("Este es un mensaje con comillas dobles.")
+nombre = "Debora"
+edad = 22
+print("Mi nombre es " + nombre + " y tengo " + str(edad) + " años.")
